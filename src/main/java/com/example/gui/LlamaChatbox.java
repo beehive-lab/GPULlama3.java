bed9b663671070363ab7223f008a5b6fb3d55b0a
svntax <endless.answers@gmail.com>
2025-06-25 05:03:54 -0400

    Remove unused import

--- src/main/java/com/example/gui/LlamaChatbox.java
+++ src/main/java/com/example/gui/LlamaChatbox.java
@@ -1,7 +1,6 @@
 package com.example.gui;
 
 import atlantafx.base.theme.CupertinoDark;
-import atlantafx.base.theme.PrimerDark;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
