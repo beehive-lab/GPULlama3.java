3b438a9fdfe5da19aed553e9c100180915fcaa40
mikepapadim <mikepapadim@hotmail.com>
2025-12-18 13:41:56 +0200

    Remove redundant TornadoVM path settings from set_paths.cmd

--- set_paths.cmd
+++ set_paths.cmd
@@ -7,10 +7,6 @@ REM Resolve the absolute path to this script's directory
 set "LLAMA_ROOT=%~dp0"
 set "LLAMA_ROOT=%LLAMA_ROOT:~0,-1%"
 
-REM Set TornadoVM root and SDK paths
-set "TORNADO_ROOT=%LLAMA_ROOT%\external\tornadovm"
-set "TORNADOVM_HOME=%TORNADO_ROOT%\bin\sdk"
-
 REM Add TornadoVM SDK and LLaMA3 bin to PATH
 set "PATH=%TORNADOVM_HOME%;%LLAMA_ROOT%;%PATH%"
 
