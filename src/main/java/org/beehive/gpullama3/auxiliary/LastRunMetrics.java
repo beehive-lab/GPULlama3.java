805dce0824d807a849ee6f265473bacdee72fa63
Landen Campbell <120332787+LegL0ngly@users.noreply.github.com>
2026-04-10 13:59:49 +0000

    Added throughput metrics, updated parameter information

--- src/main/java/org/beehive/gpullama3/auxiliary/LastRunMetrics.java
+++ src/main/java/org/beehive/gpullama3/auxiliary/LastRunMetrics.java
@@ -4,6 +4,10 @@ package org.beehive.gpullama3.auxiliary;
  * Record to store metrics from the last model run.
  * @param totalTokens The total number of tokens processed
  * @param totalNanos The total time in nanoseconds
+ * @param promptEvalCount Number of tokens in the prompt
+ * @param promptNanos Time to process the prompt in nanoseconds
+ * @param inferenceEvalCount Number of tokens in the model's response
+ * @param inferenceNanos Time to output the response in nanoseconds
  */
 public record LastRunMetrics(int totalTokens, long totalNanos, int promptEvalCount, long promptNanos, int inferenceEvalCount, long inferenceNanos) {
     /**
@@ -13,9 +17,12 @@ public record LastRunMetrics(int totalTokens, long totalNanos, int promptEvalCou
 
     /**
      * Sets the metrics for the latest run
-     *
      * @param totalTokens The total number of tokens processed
      * @param totalNanos The total time in nanoseconds
+     * @param promptEvalCount Number of tokens in the prompt
+     * @param promptNanos Time to process the prompt in nanoseconds
+     * @param inferenceEvalCount Number of tokens in the model's response
+     * @param inferenceNanos Time to output the response in nanoseconds
      */
     public static void setMetrics(int totalTokens, long totalNanos, int promptEvalCount, long promptNanos, int inferenceEvalCount, long inferenceNanos) {
         latestMetrics = new LastRunMetrics(totalTokens, totalNanos, promptEvalCount, promptNanos, inferenceEvalCount, inferenceNanos);
@@ -28,9 +35,11 @@ public record LastRunMetrics(int totalTokens, long totalNanos, int promptEvalCou
         if (latestMetrics != null) {
             double totalSeconds = latestMetrics.totalNanos() / 1_000_000_000.0;
             double promptSeconds = latestMetrics.promptNanos() / 1_000_000_000.0;
+            double prefillThroughput = latestMetrics.promptEvalCount() / promptSeconds;
             double inferenceSeconds = latestMetrics.inferenceNanos() / 1_000_000_000.0;
+            double decodeThroughput = latestMetrics.inferenceEvalCount() / inferenceSeconds;
             double tokensPerSecond = latestMetrics.totalTokens() / totalSeconds;
-            System.err.printf("\n\nAchieved tok/s: %.2f. Total tokens: %d, Total time: %d ns (%.2f s)\nPrompt tokens: %d, Prompt time: %d ns (%.2f s)\nInference tokens: %d, Inference time: %d ns (%.2f s)\n", tokensPerSecond, latestMetrics.totalTokens(), latestMetrics.totalNanos(), totalSeconds, latestMetrics.promptEvalCount(), latestMetrics.promptNanos(), promptSeconds, latestMetrics.inferenceEvalCount(), latestMetrics.inferenceNanos(), inferenceSeconds);
+            System.err.printf("\n\nAchieved tok/s: %.2f. Total tokens: %d, Total time: %d ns (%.2f s)\nPrefill throughput: %.2f tok/s, Prompt tokens: %d, Prompt time: %d ns (%.2f s)\nDecode throughput: %.2f tok/s, Inference tokens: %d, Inference time: %d ns (%.2f s)\n", tokensPerSecond, latestMetrics.totalTokens(), latestMetrics.totalNanos(), totalSeconds, prefillThroughput, latestMetrics.promptEvalCount(), latestMetrics.promptNanos(), promptSeconds, decodeThroughput, latestMetrics.inferenceEvalCount(), latestMetrics.inferenceNanos(), inferenceSeconds);
         }
     }
 }
